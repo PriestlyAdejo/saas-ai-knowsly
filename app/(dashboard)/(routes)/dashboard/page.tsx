8b3f8dc63a37854213cca62589aa7d8a4b7ff7bb
Priestly <priestlyadejo@gmail.com>
2023-07-25 17:21:08 +0100

    Added folder structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+
+const DashboardPage = () => {
+  return (
+    <Button variant="default" size="lg">
+      Click me Bitch
+    </Button>
+  );
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const LandingPage = (props: Props) => {
+  return <div>Unprotected Landing Page</div>;
+};
+
+export default LandingPage;
